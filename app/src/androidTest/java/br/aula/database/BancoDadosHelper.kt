ef06ed8fdb4f67d1383375374720154d73348983
thsferrer <thsferrer@gmail.com>
2020-01-25 09:32:33 -0300

    Aula 25.01.2020 - Agenda

--- app/src/androidTest/java/br/aula/database/BancoDadosHelper.kt
+++ app/src/androidTest/java/br/aula/database/BancoDadosHelper.kt
@@ -2,11 +2,18 @@ package br.aula.database
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
+import br.aula.database.ConstantsDb.CONTATOS_DB_TABLE
 import org.jetbrains.anko.db.*
 
 class BancoDadosHelper(context: Context) :
     ManagedSQLiteOpenHelper(ctx = context ,
         name = "agenda.db",  version = 1) {
+    private val scriptSQLCreate = arrayOf(
+        "INSERT INTO $CONTATOS_DB_TABLE VALUES(1,'fernando.collor@gmail.com','Fernando Collor','R. Assungui, 27, Cursino, São Paulo, 04131-000, Brasil',800200300,NULL,'www.google.com.br',NULL);",
+        "INSERT INTO $CONTATOS_DB_TABLE VALUES(2,'dilma@gmail.com','Dilma','R. José Cocciuffo, 90 - Cursino, São Paulo, 04121-120, Brasil',800235468,NULL,'www.uol.com.br',NULL);",
+        "INSERT INTO $CONTATOS_DB_TABLE VALUES(3,'lula@gmail.com','Lula','R. José Cocciuffo, 56 - Cursino, São Paulo, 04121-120, Brasil',80023587,NULL,'www.google.com',NULL);",
+        "INSERT INTO $CONTATOS_DB_TABLE VALUES(4,'maluf@gmail.com','Maluf','R. Camilo José, 48 - Cursino, São Paulo, 04125-140, Brasil',800025774,NULL,'www.uol.com.br',NULL);")
+
     //singleton da classe (obj é criado 1x e reutilizado)
     companion object {
         private var instance: BancoDadosHelper? = null
@@ -31,8 +38,14 @@ class BancoDadosHelper(context: Context) :
             "site" to TEXT,
             "foto" to TEXT
         )
+        // insere dados iniciais na tabela
+        scriptSQLCreate.forEach {sql ->
+            db.execSQL(sql)
+        }
     }
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         TODO("not implemented")
     }
 }
+
+    val Context.database: BancoDadosHelper get() = BancoDadosHelper.getInstance(getApplicationContext())
---
+++ app/src/androidTest/java/br/aula/database/ConstantsDb.kt
@@ -0,0 +1,6 @@
+package br.aula.database
+
+object ConstantsDb {
+    const val CONTATOS_DB_NAME = "agenda.db"
+    const val CONTATOS_DB_TABLE = "agenda"
+}
