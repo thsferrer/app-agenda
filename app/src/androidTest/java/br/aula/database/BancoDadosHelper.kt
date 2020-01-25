ca8d5423a418b5fbcdd320e197525cf1680a5ee1
thsferrer <thsferrer@gmail.com>
2020-01-25 08:58:55 -0300

    Aula 11.01.2020 - Agenda

--- app/src/androidTest/java/br/aula/database/BancoDadosHelper.kt
+++ app/src/androidTest/java/br/aula/database/BancoDadosHelper.kt
@@ -2,14 +2,23 @@ package br.aula.database
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
-import org.jetbrains.anko.db.ManagedSQLiteOpenHelper
+import org.jetbrains.anko.db.*
 
 class BancoDadosHelper(context: Context) :
     ManagedSQLiteOpenHelper(ctx = context ,
         name = "agenda.db",  version = 1) {
 
     override fun onCreate(db: SQLiteDatabase) {
-        // Criação de tabelas
+        db.createTable("agenda", true,
+            "id" to INTEGER + PRIMARY_KEY + UNIQUE,
+            "email" to TEXT,
+            "nome" to TEXT,
+            "endereco" to TEXT,
+            "telefone" to INTEGER,
+            "datanascimento" to INTEGER,
+            "site" to TEXT,
+            "foto" to TEXT
+        )
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
